fafb23bf73a21d1c5a409fa80a53dc171130c52c
ronan <ronanluo@boyaa.com>
2019-02-19 18:20:18 +0800

    feat: add note of golang nil interface

---
+++ go/nil_interface.go
@@ -0,0 +1,27 @@
+/*
+@Time : 2018/12/3 17:39
+@Author : RonanLuo
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+type Event struct {
+	id   int32
+	uid  uint64
+	flag int32
+	data interface{}
+}
+
+var eventMap = map[int]interface{}{}
+
+func main() {
+	var e *Event
+	e = nil
+	eventMap[1] = e
+	eventMap[2] = nil
+	fmt.Println(e == nil, eventMap[1] == nil, eventMap[2] == nil)
+}
